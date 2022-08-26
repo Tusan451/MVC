254e26bd79b8a4afeed8e1d4a0cad5a131f9364e
Oleg Fedotov <trevor706@gmail.com>
2022-08-26 19:09:48 +0300

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Olegio on 26.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
