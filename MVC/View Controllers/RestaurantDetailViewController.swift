abd019fa1d4914cec7e765fcf83d04a6ab453ad1
Oleg Fedotov <trevor706@gmail.com>
2022-08-29 19:01:56 +0300

    RestaurantDetail outlets

--- MVC/View Controllers/RestaurantDetailViewController.swift
+++ MVC/View Controllers/RestaurantDetailViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
 
+    @IBOutlet var restaurantImage: UIImageView!
+    @IBOutlet var restaurantName: UILabel!
+    @IBOutlet var restaurantRating: UILabel!
+    @IBOutlet var restaurantCost: UILabel!
+    @IBOutlet var restaurantAddress: UILabel!
+    @IBOutlet var restaurantInfo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
